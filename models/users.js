4d1239aee952edd2054fde6deafa43965f9b5d99
jacoblimzm <jacob_lim@live.com>
2021-04-16 11:42:57 +0800

    create user schema and model

---
+++ models/users.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => {
+        return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(v);
+      },
+    },
+  },
+  address: { type: String },
+  createdAt: Number,
+  updatedAt: Number,
+});
+
+const User = mongoose.model("user", userSchema);
+
+
+module.exports = User;
