c9ef6a6b916f8e01e365d429028e1c85bfb5008e
jacoblimzm <jacob_lim@live.com>
2021-04-16 13:36:06 +0800

    update users model with timestamps

--- models/users.js
+++ models/users.js
@@ -1,31 +1,39 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    validate: {
-      validator: (v) => {
-        return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(v);
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: [true, "Username already exists"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required!"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required!"],
+      validate: {
+        validator: function (v) {
+          return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+            v
+          );
+        },
+        message: (props) => `${props.value} is not a valid email address!`,
       },
     },
+    address: { type: String },
   },
-  address: { type: String },
-  createdAt: Number,
-  updatedAt: Number,
-});
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
 
 const User = mongoose.model("user", userSchema);
-
 
 module.exports = User;
