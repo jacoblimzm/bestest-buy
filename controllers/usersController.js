34e41e1d623e77cdd41b9d2e294cdc2c279d765f
jacoblimzm <jacob_lim@live.com>
2021-04-16 13:38:23 +0800

    install packages for sessions and passport

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,23 +2,36 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const User = require("../models/users");
 
-
-
 // CONSTANTS
 const users = express.Router();
-
+const saltRounds = 10;
 
 // ROUTES
+
+// CREATE user route
 users.post("/", (req, res) => {
-    
-    const newUser = req.body;
-})
+  const newUser = req.body;
+  console.log(newUser);
 
+  bcrypt.hash(newUser.password, saltRounds, (err, hash) => {
+    if (err) {
+      console.log(err);
+    } else { // if all is successful, change the password to the user to the hash.
+      newUser.password = hash;
+      User.create(newUser, (err, createdUser) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(createdUser);
+          res.send(createdUser) // send the createdUser obj back to client as a response.
+        }
+      });
+    }
+  });
+});
 
-users.put("/")
+users.put("/");
 
-
-users.delete("/")
-
+users.delete("/");
 
 module.exports = users;
