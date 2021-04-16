445707e7c4da3af7fac6ad151580d8021ca0f60b
jacoblimzm <jacob_lim@live.com>
2021-04-16 16:29:59 +0800

    set up mock forms to register, log in, get user details

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    const username = e.target.username.value;
+    const email= e.target.email.value;
+    const password = e.target.password.value;
+
+    // upon submit, an API POST request must be made to the backend to /usersbackend
+    axios
+      .post("/usersbackend", {
+        username,
+        email,
+        password,
+      })
+      .then((res) => {
+        console.log(res.data); // the backend responds with a json object!!
+        // handleAddBookmark(res.data); // need to haul the user object to the TOP;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleLogIn = (e) => {
+    e.preventDefault();
+  };
+  const handleGetUser = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Register</h1>
+        <form onSubmit={handleRegister}>
+          <input type="text" name="username" placeholder="username" />
+          <input type="email" name="email" placeholder="email" />
+          <input type="password" name="password" placeholder="password" />
+          <input type="submit" name="submit" value="Register" />
+        </form>
+      </div>
+      <div>
+        <h1>Log In</h1>
+        <form onSubmit={handleLogIn}>
+          <input type="text" name="username" placeholder="username" />
+          <input type="password" name="password" placeholder="password" />
+          <input type="submit" name="submit" value="Log In" />
+        </form>
+      </div>
+      <div>
+        <h1>Get User</h1>
+        <form onSubmit={handleGetUser}>
+          <input type="text" name="username" placeholder="username" />
+          <input type="password" name="password" placeholder="password" />
+          <input type="submit" name="submit" value="Get User" />
+        </form>
+      </div>
     </div>
   );
 }
