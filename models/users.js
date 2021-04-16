c3904b025ab23174bbfb0e81db4140b87e06303c
jacoblimzm <jacob_lim@live.com>
2021-04-16 17:42:06 +0800

    add isAdmin property and format code

--- models/users.js
+++ models/users.js
@@ -1,33 +1,32 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      //   unique: [true, "Username already exists"],
-    },
-    password: {
-      type: String,
-      required: [true, "Password is required!"],
-    },
-    email: {
-      type: String,
-      required: [true, "Email is required!"],
-      validate: {
-        validator: function (v) {
-          return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
-            v
-          );
-        },
-        message: (props) => `${props.value} is not a valid email address!`,
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    //   unique: [true, "Username already exists"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required!"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required!"],
+    validate: {
+      validator: function (v) {
+        return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+          v
+        );
       },
+      message: (props) => `${props.value} is not a valid email address!`,
     },
-    address: { type: String },
-    createdAt: { type: Date, default: Date.now },
-  }
-);
+  },
+  address: { type: String },
+  isAdmin: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now },
+});
 
 const User = mongoose.model("user", userSchema);
 
