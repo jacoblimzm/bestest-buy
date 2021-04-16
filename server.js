2f43c288742777b0c478d993fa0bc6b3b8b05d62
jacoblimzm <jacob_lim@live.com>
2021-04-16 13:49:59 +0800

    add cors and session middleware

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
 require("dotenv-safe").config();
 const express = require("express");
-// express session
 const mongoose = require("mongoose");
+const session = require("express-session");
+const cors = require("cors");
 
-// CONSTANTS
+
+// --------------------------------------- CONSTANTS ---------------------------------------
 const app = express();
 const PORT = process.env.PORT;
+const SECRET = process.env.SECRET;
 
-// MIDDLEWARE
-app.use(express.urlencoded({ extended: true })); // for parsing form information.
+// --------------------------------------- MIDDLEWARE ---------------------------------------
 app.use(express.json()); // for parsing raw json information
+app.use(express.urlencoded({ extended: true })); // for parsing form information.
+app.use(cors({
+    origin: "http://localhost:3000", //location of the react app that is connect
+    credentials: true
+}))
+app.use(session({
+    secret: SECRET,
+    resave: false,
+    saveUninitialized: false,
+}))
 
-
-// CONTROLLERS
+// --------------------------------------- CONTROLLERS ---------------------------------------
 const usersController = require("./controllers/usersController");
 app.use("/usersbackend", usersController); 
 
-// CONNECTIONS
+// --------------------------------------- CONNECTIONS ---------------------------------------
 app.listen(PORT, () => {
   console.log("server is listening at port", PORT);
 });
