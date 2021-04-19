bd74720cd4c696e9378b9aaf43bb31935bb4b51b
jacoblimzm <jacob_lim@live.com>
2021-04-19 23:11:42 +0800

    add action types for reducer

---
+++ client/src/actions/types.js
@@ -0,0 +1,14 @@
+export const GET_ITEMS = "GET_ITEMS";
+export const ADD_ITEM = "ADD_ITEM";
+export const DELETE_ITEM = "DELETE_ITEM";
+export const UPDATE_ITEM = "UPDATE_ITEM";
+
+export const GET_CART = "GET_CART";
+export const ADD_TO_CART = "ADD_TO_CART";
+export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+
+export const GET_ORDERS = "GET_ORDERS";
+export const CHECKOUT = "CHECKOUT";
+
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
