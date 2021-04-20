4bf92b3d78367058b5ed01a9b8b63b7e28cff79f
jacoblimzm <jacob_lim@live.com>
2021-04-21 00:13:49 +0800

    minor format fix

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,25 +1,4 @@
 import axios from "axios";
-<<<<<<< HEAD
-import { useEffect, useState } from "react";
-//material-ui imports
-import Button from "@material-ui/core/Button";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Typography from "@material-ui/core/Typography";
-
-//To configure material-ui
-const useStyles = makeStyles({
-  root: {
-    maxWidth: 345,
-  },
-  media: {
-    height: 140,
-  },
-=======
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
@@ -42,60 +21,9 @@ const useStyles = makeStyles({
     media: {
         height: 200,
     },
->>>>>>> 171d6083995a7ddf8ec0401c80430520a69a747a
 });
 
 export default function Home() {
-<<<<<<< HEAD
-  const [Category, setCategory] = useState([]);
-  const [categories, setCategories] = useState({});
-
-  useEffect(() => {
-    axios
-      .get("/productsbackend")
-      .then((res) => {
-        console.log(res.data);
-        setCategory(res.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-
-  const handleClick = (event) => {
-    event.preventDefault();
-    console.log("Go to category page");
-  };
-  const classes = useStyles();
-
-  return (
-    <>
-      <h4>Your shopping spree starts now.</h4>
-      <Button>Shop now</Button>
-      <Button>Scroll up</Button>
-      {Category.map((categories) => {
-        return (
-          <Card className={classes.root}>
-            <CardActionArea onClick={handleClick}>
-              <CardMedia
-                className={classes.media}
-                image="https://picsum.photos/500/500"
-                title={categories.category}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  {categories.category}
-                </Typography>
-              </CardContent>
-              <CardActions></CardActions>
-            </CardActionArea>
-          </Card>
-        );
-      })}
-    </>
-  );
-}
-=======
     const [Category, setCategory] = useState([]);
     const [categories, setCategories] = useState({});
     useEffect(() => {
@@ -106,7 +34,6 @@ export default function Home() {
             console.log(error);
         })
     }, []);
-
     const handleClick = (event) => {
         event.preventDefault();
         console.log("Go to category page");
@@ -148,4 +75,3 @@ export default function Home() {
         </>
     )
 }
->>>>>>> 171d6083995a7ddf8ec0401c80430520a69a747a
