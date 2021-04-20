0ca966388d1e2565f70f08a53a0bcf04137209a0
Eleanor <Eleanor>
2021-04-20 12:14:04 +0800

    attempt to pull master

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,14 @@ import { useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import TestForm from "./pages/TestForm";
 
+//Pages
+import Home from './pages/Home'
+
 function App() {
   return (
     <Router>
       <div className="App">
+        <Home />
         <Switch>
           <Route exact path="/">
             <TestForm />
