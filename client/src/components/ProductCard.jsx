08c9b2580651b01010cb6c05c6b125dd72842ca0
jacoblimzm <jacob_lim@live.com>
2021-04-20 16:45:27 +0800

    fix formatting

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -11,74 +11,6 @@ import Typography from "@material-ui/core/Typography";
 import { Link } from "react-router-dom";
 
 const ProductCard = ({ _id, name, description, brand, price, image }) => {
-  const product = [
-    {
-      _id: "607e480f95c64f67220c430e",
-      name: "Vitamin-C",
-      brand: "Swisse",
-      description: "100 capsule",
-      category: "Health & Wellness",
-      image: "x",
-      price: 70,
-      createdAt: "2021-04-20T03:18:39.012Z",
-      __v: 0,
-    },
-    {
-      _id: "607e480f95c64f67220c430b",
-      name: "Macbook pro",
-      brand: "Apple",
-      description: "Macbook pro 13 inch",
-      category: "Electronics",
-      image: "x",
-      price: 2000,
-      createdAt: "2021-04-20T03:18:39.011Z",
-      __v: 0,
-    },
-    {
-      _id: "607e480f95c64f67220c430a",
-      name: "T-Shirt",
-      brand: "Zara",
-      description: "White crew neck",
-      category: "Fashion",
-      image: "x",
-      price: 20,
-      createdAt: "2021-04-20T03:18:39.009Z",
-      __v: 0,
-    },
-    {
-      _id: "607e480f95c64f67220c430f",
-      name: "Facial Cleanser",
-      brand: "Innisfree",
-      description: "Whitening, 100ml",
-      category: "Beauty & personal care",
-      image: "x",
-      price: 28,
-      createdAt: "2021-04-20T03:18:39.012Z",
-      __v: 0,
-    },
-    {
-      _id: "607e480f95c64f67220c430d",
-      name: "Ice-cream",
-      brand: "Häagen Dazs",
-      description: "Chocolate",
-      category: "Food & Beverages",
-      image: "x",
-      price: 13,
-      createdAt: "2021-04-20T03:18:39.012Z",
-      __v: 0,
-    },
-    {
-      _id: "607e480f95c64f67220c430c",
-      name: "Air-fryer",
-      brand: "Philips",
-      description: "Black 5 litre",
-      category: "Household",
-      image: "x",
-      price: 700,
-      createdAt: "2021-04-20T03:18:39.011Z",
-      __v: 0,
-    },
-  ];
 
   const useStyles = makeStyles({
     root: {
@@ -89,6 +21,7 @@ const ProductCard = ({ _id, name, description, brand, price, image }) => {
     },
     link: {
         textDecoration: "none",
+        color: "#000"
     }
   });
   const classes = useStyles();
@@ -98,7 +31,7 @@ const ProductCard = ({ _id, name, description, brand, price, image }) => {
       <Grid item sm={4} md={3}>
         <Card className={classes.root}>
           <CardActionArea>
-            <Link to="/" className={classes.link}>
+            <Link to={`/products/${_id}`} className={classes.link}>
               <CardMedia
                 className={classes.media}
                 image="https://picsum.photos/500/500"
@@ -116,7 +49,7 @@ const ProductCard = ({ _id, name, description, brand, price, image }) => {
                 >
                   ({brand})
                 </Typography>
-                <Typography variant="body2" color="textPrimary" component="p">
+                <Typography variant="h6" color="textPrimary" component="p">
                   ${price}
                 </Typography>
               </CardContent>
