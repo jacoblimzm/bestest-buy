8de4bef52c1cfafe2babd98180567889a6d12fed
jacoblimzm <jacob_lim@live.com>
2021-04-20 15:31:00 +0800

    import UserContext into App

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,27 @@ import axios from "axios";
 import "./App.css";
 import { useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import TestForm from "./pages/TestForm";
 import CartProvider from "./context/CartProvider";
+import Container from "@material-ui/core/Container";
+import ProductList from "./pages/ProductList";
+
+import UserProvider from "./context/UserProvider";
 
 function App() {
   return (
     <Router>
       <CartProvider>
-        <div className="App">
-          <Switch>
-            <Route exact path="/">
-              <TestForm />
-            </Route>
-          </Switch>
-        </div>
+        <UserProvider>
+          <Container maxWidth="lg">
+            <div className="App">
+              <Switch>
+                <Route exact path="/">
+                  <ProductList />
+                </Route>
+              </Switch>
+            </div>
+          </Container>
+        </UserProvider>
       </CartProvider>
     </Router>
   );
