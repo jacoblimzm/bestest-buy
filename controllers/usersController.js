284e7fe5f38b33d5c3f5fe3415de24ce8f11dc5a
jacoblimzm <jacob_lim@live.com>
2021-04-21 15:14:29 +0800

    use joi validation schema in user registration

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const User = require("../models/users");
 const isUserAuthenticated = require("../passport/middleware");
+const { userValidSchema } = require("../validation/validationSchemas");
 
 // --------------------------------------- CONSTANTS ---------------------------------------
 const users = express.Router();
@@ -19,51 +20,74 @@ users.get("/", isUserAuthenticated, (req, res) => {
 // CREATE User route
 users.post("/", (req, res) => {
   const newUser = req.body;
-  console.log(newUser);
+  // console.log(newUser);
+  const result = userValidSchema.validate(newUser); // result has two properties, "error" and "value". "error" is undefined if no error. value will be user object
+  // console.log(result.error.message);
 
-  // do validation checks: if fields empty, or if user already exists.
-  bcrypt.hash(newUser.password, saltRounds, (err, hash) => {
-    if (err) {
-      res.send(err);
-    } else {
-      // if all is successful, change the password to the user to the hash.
-      newUser.password = hash;
-      User.create(newUser, (err, createdUser) => {
-        if (err) {
-          res.send(err);
-        } else {
-          console.log(createdUser);
-          res.send(createdUser); // send the createdUser obj back to client as a response.
-          // TO DO: authenticate user immediately and log them in after registration?
-        }
-      });
-    }
-  });
+  if (result.error) {
+    // 1st Level Validation by Joi. Check that fields are valid and not empty.
+    res.send({ message: result.error.message });
+  } else {
+    // do validation checks: if fields empty, or if user already exists.
+    User.findOne({ username: result.value.username }, (err, foundUser) => {
+      // 2nd Level Validation. Check that user does not currently exist.
+      if (foundUser) {
+        res.send({
+          message: `'${result.value.username}' has already been registered!`,
+        });
+      } else {
+        bcrypt.hash(result.value.password, saltRounds, (err, hash) => {
+          if (err) {
+            res.send(err);
+          } else {
+            // if all is successful, change the password to the user to the hash.
+            result.value.password = hash;
+            User.create(result.value, (err, createdUser) => {
+              if (err) {
+                res.send(err);
+              } else {
+                console.log(createdUser);
+                res.send(createdUser); // send the createdUser obj back to client as a response.
+                // TO DO: authenticate user immediately and log them in after registration?
+              }
+            });
+          }
+        });
+      }
+    });
+  }
 });
+
+// try {
+// } catch (error) {
+//   if (error.isJoi === true) {
+//     error.status = 422;
+//     next(error);
+//   }
+// }
 
 // EDIT User Route
 users.put("/:id", isUserAuthenticated, (req, res) => {
-
   const editedUser = req.body;
-    bcrypt.hash(editedUser.password, saltRounds, (err, hash) => {
-      if (err) {
-        res.send(err);
-      } else {
-        editedUser.password = hash;
-        User.findByIdAndUpdate(
-          req.params.id,
-          { ...editedUser },
-          { new: true },
-          (err, updatedUser) => {
-            if (err) {
-              res.send({ message: "Did not update successfully." });
-            } else {
-              res.send(updatedUser);
-            }
+  bcrypt.hash(editedUser.password, saltRounds, (err, hash) => {
+    if (err) {
+      res.send(err);
+    } else {
+      editedUser.password = hash;
+      User.findByIdAndUpdate(
+        req.params.id,
+        { ...editedUser },
+        { new: true },
+        (err, updatedUser) => {
+          if (err) {
+            res.send({ message: "Did not update successfully." });
+          } else {
+            res.send(updatedUser);
           }
-        );
-      }
-    });
+        }
+      );
+    }
+  });
 
   // if (req.isAuthenticated()) {
   //   //built-in method of passport to check if current user is authenticated. returns a boolean
@@ -73,7 +97,6 @@ users.put("/:id", isUserAuthenticated, (req, res) => {
 });
 
 users.delete("/:id", isUserAuthenticated, (req, res) => {
-
   User.findByIdAndDelete(req.params.id, (err, deletedUser) => {
     if (err) {
       res.send(err);
@@ -84,7 +107,7 @@ users.delete("/:id", isUserAuthenticated, (req, res) => {
     }
   });
 
-  // if (req.isAuthenticated()) {  
+  // if (req.isAuthenticated()) {
   //   //built-in method of passport to check if current user is authenticated. returns a boolean
   // } else {
   //   res.send({ message: "Request is denied. Not a valid login session." });
