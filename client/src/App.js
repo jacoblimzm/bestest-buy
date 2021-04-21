94f3d413ab656a1b87153f1bdf86c533b2a8cafd
shinx2 <limkah_sh@hotmail.com>
2021-04-21 16:23:20 +0800

    accept app.js changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,5 @@
 import "./App.css";
-<<<<<<< HEAD
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import CartProvider from "./context/CartProvider";
-import UserProvider from "./context/UserProvider";
-import Nav from "./components/Nav";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
-=======
 import { Switch, Route } from "react-router-dom";
->>>>>>> 618ecdb7f01e7da1346724319961e62a759b948c
 import Container from "@material-ui/core/Container";
 import ProductList from "./pages/ProductList";
 
@@ -20,14 +11,6 @@ import Cart from "./pages/Cart"
 function App() {
 
   return (
-<<<<<<< HEAD
-    <>
-    <Router>
-      <UserProvider>
-      <CartProvider>
-        < Nav/>
-=======
->>>>>>> 618ecdb7f01e7da1346724319961e62a759b948c
           <Container maxWidth="lg">
             <div className="App">
               <Switch>
@@ -48,13 +31,6 @@ function App() {
               </Switch>
             </div>
           </Container>
-<<<<<<< HEAD
-      </CartProvider>
-      </UserProvider>
-    </Router>
-    </>
-=======
->>>>>>> 618ecdb7f01e7da1346724319961e62a759b948c
   );
 }
 
