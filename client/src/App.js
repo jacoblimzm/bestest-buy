4b7324aaa0eaf782dff747c4882d1f95260c7940
jacoblimzm <jacob_lim@live.com>
2021-04-21 15:54:46 +0800

    comment out components without imports

--- client/src/App.js
+++ client/src/App.js
@@ -6,31 +6,30 @@ import ProductList from "./pages/ProductList";
 //Pages
 import Home from "./pages/Home";
 import ProductDetails from "./pages/ProductDetails";
-import Cart from "./pages/Cart"
+import Cart from "./pages/Cart";
 
 function App() {
-
   return (
-          <Container maxWidth="lg">
-            <div className="App">
-              <Switch>
-                <Route exact path="/">
-                  <Home />
-                </Route>
-                <Route exact path="/login" component={Login}/>
-                <Route exact path="/signup" component={SignUp}/>
-                <Route path="/product/:productId">
-                  <ProductDetails />
-                </Route>
-                <Route path="/products/:category">
-                  <ProductList />
-                </Route>
-                <Route exact path="/cart">
-                  <Cart />
-                </Route>
-              </Switch>
-            </div>
-          </Container>
+    <Container maxWidth="lg">
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          {/* <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={SignUp} /> */}
+          <Route path="/product/:productId">
+            <ProductDetails />
+          </Route>
+          <Route path="/products/:category">
+            <ProductList />
+          </Route>
+          <Route exact path="/cart">
+            <Cart />
+          </Route>
+        </Switch>
+      </div>
+    </Container>
   );
 }
 
