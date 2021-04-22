efdad9b233a7730dc48d2533f7663690aa707fc8
jacoblimzm <jacob_lim@live.com>
2021-04-23 00:48:57 +0800

    add error message

--- client/src/components/controls/Input.jsx
+++ client/src/components/controls/Input.jsx
@@ -1,7 +1,7 @@
 import { TextField } from "@material-ui/core";
 
 const Input = (props) => {
-  const { name, type, label, value, handleInputChange } = props;
+  const { name, type, label, value, handleInputChange, errorMessage=null } = props;
   return (
     <TextField
       fullWidth
@@ -15,11 +15,11 @@ const Input = (props) => {
       autoFocus
       value={value}
       onChange={handleInputChange}
-      // Step 3: In ALL the input fields, conditionally render the "error" property and the "helperText" property with the error message
-    //   {...(errors.name && {
-    //     error: true,
-    //     helperText: errors.name,
-    //   })}
+      // Step 3: In ALL the input fields, conditionally render the "error" property and the "helperText" property with the error message if an error message exists
+      {...(errorMessage && {
+        error: true,
+        helperText: errorMessage,
+      })}
     />
   );
 };
