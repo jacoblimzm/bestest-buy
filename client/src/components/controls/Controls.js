d34a4bd456c7704fb83a4c8d39e0c948a9424f09
jacoblimzm <jacob_lim@live.com>
2021-04-23 01:48:20 +0800

    import Custom Button

--- client/src/components/controls/Controls.js
+++ client/src/components/controls/Controls.js
@@ -1,9 +1,11 @@
 import Input from "./Input"
 import DropDown from "./DropDown"
+import CustomButtom from "./CustomButton"
 
 const Controls = {
     Input,
     DropDown,
+    CustomButtom
 }
 export default Controls;
 
