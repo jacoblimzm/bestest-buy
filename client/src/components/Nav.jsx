3a71eceb48413da8c1f78eacaf818aa22f2a294f
shinx2 <limkah_sh@hotmail.com>
2021-04-23 01:46:05 +0800

    add hide/show button, profile page, login checks

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,104 +1,181 @@
-import React, {useEffect, useState, useContext} from "react";
-import axios from 'axios';
-import { makeStyles } from '@material-ui/core/styles';
-import {AppBar, Toolbar, IconButton, Typography, Button, MenuItem, MenuClose, Menu} from "@material-ui/core";
-import {UserContext} from "../context/UserProvider";
+import React, { useEffect, useState, useContext } from "react";
+import axios from "axios";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+  MenuItem,
+  MenuClose,
+  Menu,
+} from "@material-ui/core";
+import { UserContext } from "../context/UserProvider";
 import { useHistory } from "react-router-dom";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import { LOGOUT_SUCCESS } from "../actions/types";
 
 const Nav = () => {
   const history = useHistory();
   const classes = makeStyles();
-  const {state} = useContext(UserContext);
-  const [categories,setCategories] =useState([]);
-  const [anchorEl, setAnchorEl] = useState(null);
+  const user = useContext(UserContext);
+  const [categories, setCategories] = useState([]);
+  const [profileAnchor, setProfileAnchor] = useState(null);
+  const [catAnchor, setCatAnchor] = useState(null);
 
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
+  const handleClickCat = (event) => {
+    setCatAnchor(event.currentTarget);
   };
 
-  const handleClose = () => {
-    setAnchorEl(null);
+  const handleCloseCat = () => {
+    setCatAnchor(null);
+  };
+
+  const handleClickProfile = (event) => {
+    setProfileAnchor(event.currentTarget);
+  };
+
+  const handleCloseProfile = () => {
+    setProfileAnchor(null);
   };
 
   //useEffect, callling of APIS
   useEffect(() => {
-    axios.get("/categoriesbackend").then((res) => {
+    axios
+      .get("/categoriesbackend")
+      .then((res) => {
         console.log(res.data);
         setCategories(res.data);
-    }).catch((error) => {
+      })
+      .catch((error) => {
         console.log(error);
-    })
-}, []);
+      });
+  }, []);
 
-return (
+  return (
+    <AppBar position="sticky">
+      <Toolbar>
+        <IconButton
+          edge="start"
+          className={classes.menuButton}
+          color="inherit"
+          aria-label="menu"
+        ></IconButton>
 
-  <AppBar position="sticky">
-  <Toolbar>
-    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-    </IconButton>
+        <Button
+          color="inherit"
+          onClick={() => {
+            history.push("/");
+          }}
+        >
+          Bestest-Buy
+        </Button>
 
-    <Button color="inherit" onClick={()=>{history.push("/")}}>
-    Bestest-Buy
-    </Button>
+        <div style={{ flexGrow: 1 }}></div>
+        <Button
+          color="inherit"
+          aria-controls="simple-menu"
+          aria-haspopup="true"
+          onClick={handleClickCat}
+        >
+          Shop Category <ArrowDropDownIcon fontSize="small" />
+        </Button>
 
-
-    <div style={{flexGrow:1}}></div>
-
-    <Button color="inherit" aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-      Shop Category
-    </Button>
-
-    <Menu
-      id="simple-menu"
-      anchorEl={anchorEl}
-      keepMounted
-      open={Boolean(anchorEl)}
-      onClose={handleClose}
-    >
-
-      {
-        categories.map((category)=> {
+        <Menu
+          id="simple-menu"
+          anchorEl={catAnchor}
+          keepMounted
+          open={Boolean(catAnchor)}
+          onClose={handleCloseCat}
+        >
+          {categories.map((category) => {
             return (
-                <>
-                <MenuItem onClick={(e)=>{
-                  history.push(`/products/${category.category}`);}}>
-                 {/* <MenuItem 
-                 component={Link}    
-                 to={`/products/${category.category}`}> */}
-                        {category.category}
-                 </MenuItem>
-                </>
+              <>
+                <MenuItem
+                  onClick={(e) => {
+                    history.push(`/products/${category.category}`);
+                  }}
+                >
+                  {category.category}
+                </MenuItem>
+              </>
             );
-        })
-      }
-      
-    </Menu>
-
-    <Button color="inherit" onClick={()=>{history.push("/signup")}}>Sign Up</Button>
-    <Button color="inherit" onClick={()=>{history.push("/login")}}>Login</Button>
-  
-    {(state.isAuthenticated) && (
-      <>
-      <Button color="inherit" aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick}>
-      {state.user.username}</Button>
-<Menu
-  id="simple-menu"
-  anchorEl={anchorEl}
-  keepMounted
-  open={Boolean(anchorEl)}
-  onClose={handleClose}
->
-  <MenuItem onClick={()=>{history.push("/myprofile")}}>Profile</MenuItem>
-  <MenuItem onClick={()=>{history.push("/cart")}}>My Cart</MenuItem>
-  <MenuItem onClick={()=>{history.push("/orders")}}>My Order</MenuItem>
-  <MenuItem onClick={()=>{history.push("/")}}>Logout</MenuItem>
-</Menu>
-</>
-)}
-  </Toolbar>
-</AppBar>
-
-)
-}
+          })}
+        </Menu>
+        {!user.state.isAuthenticated && (
+          <Button
+            color="inherit"
+            onClick={() => {
+              history.push("/signup");
+            }}
+          >
+            Sign Up
+          </Button>
+        )}
+        {!user.state.isAuthenticated && (
+          <Button
+            color="inherit"
+            onClick={() => {
+              history.push("/login");
+            }}
+          >
+            Login
+          </Button>
+        )}
+        {user.state.isAuthenticated && (
+          <>
+            <Button
+              color="inherit"
+              aria-controls="simple-menu"
+              aria-haspopup="true"
+              onClick={handleClickProfile}
+            >
+              {user.state.user.username}
+              <ArrowDropDownIcon fontSize="small" />
+            </Button>
+            <Menu
+              id="simple-menu"
+              anchorEl={profileAnchor}
+              keepMounted
+              open={Boolean(profileAnchor)}
+              onClose={handleCloseProfile}
+            >
+              <MenuItem
+                onClick={() => {
+                  history.push("/myprofile");
+                }}
+              >
+                Profile
+              </MenuItem>
+              <MenuItem
+                onClick={() => {
+                  history.push("/cart");
+                }}
+              >
+                My Cart
+              </MenuItem>
+              <MenuItem
+                onClick={() => {
+                  history.push("/orders");
+                }}
+              >
+                My Order
+              </MenuItem>
+              <MenuItem
+                onClick={() => {
+                  history.push("/");
+                  user.dispatch({ type: LOGOUT_SUCCESS, payload: {} });
+                }}
+              >
+                Logout
+              </MenuItem>
+            </Menu>
+          </>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Nav;
--- client/src/pages/MyProfile.jsx
+++ client/src/pages/MyProfile.jsx
@@ -1,52 +1,105 @@
-import React, {useContext, useState} from 'react';
-import {Grid, Typography, TextField, FormControlLabel, Checkbox, Button, } from '@material-ui/core';
+import React, { useContext, useState, useEffect } from "react";
+import {
+  Grid,
+  Typography,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Button,
+} from "@material-ui/core";
 import { UserContext } from "../context/UserProvider";
-import axios from 'axios';
-import { makeStyles } from '@material-ui/core/styles';
-
+import axios from "axios";
+import { makeStyles } from "@material-ui/core/styles";
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+import Alert from "@material-ui/lab/Alert";
+import { useHistory } from "react-router-dom";
+import { LOGOUT_SUCCESS } from "../actions/types";
 
 const MyProfile = () => {
-    const classes = makeStyles();
-    const user = useContext(UserContext);
-    console.log(user);
-    const [inputValues, setInputValues] = useState({
-        username: "",
-        password: "",
-        email: "",
-        address: "",
-     }); 
- 
+  const history = useHistory();
+  const classes = makeStyles();
+  const user = useContext(UserContext);
+  const [open, setOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("Server Errror!");
+  const [inputValues, setInputValues] = useState({
+    username: user.state.user.username,
+    password: user.state.user.password,
+    email: user.state.user.email,
+    address: user.state.user.address,
+  });
+  const [isEdit, setIsEdit] = useState(false);
+  const [isInputValid, setIsInputValid] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
 
-    const handleChangeInputValues = attr => event =>{
-        setInputValues({...inputValues, [attr]: event.target.value})
-    };
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpen(false);
+  };
 
-    const handleEdit = (e) => {
-        e.preventDefault();
-      };
-    
-      const handleSave = (e) => {
-        e.preventDefault();
-        axios
-      .put("/sessionsbackend", {
-        "username": inputValues.username,
-        "password": inputValues.password,
-        "email": inputValues.email,
-        "address": inputValues.address
+  useEffect(() => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (inputValues.username.length < 5) {
+      setIsInputValid(true);
+      setAlertMessage("Username must be more than 5 characters");
+    } else if (inputValues.password.length < 8) {
+      setIsInputValid(true);
+      setAlertMessage("Password must be more than 8 characters");
+    } else if (re.test(inputValues.email) === false) {
+      setIsInputValid(true);
+      setAlertMessage("Please provide valid email");
+    } else if (inputValues.address.length === 0) {
+      setIsInputValid(true);
+      setAlertMessage("Address cannot be empty!");
+    } else {
+      setIsInputValid(false);
+      setAlertMessage("");
+    }
+  }, [inputValues]);
+
+  const handleChangeInputValues = (attr) => (event) => {
+    setInputValues({ ...inputValues, [attr]: event.target.value });
+  };
+
+  const handleEdit = (e) => {
+    e.preventDefault();
+    setIsEdit(!isEdit);
+  };
+
+  const handleSave = (e) => {
+    e.preventDefault();
+    setOpen(true);
+    axios
+      .put(`/usersbackend/${user.state.user._id}`, {
+        username: inputValues.username,
+        password: inputValues.password,
+        email: inputValues.email,
+        address: inputValues.address,
       })
       .then((res) => {
         console.log(res.data);
-          })
+        if (!res.data.message) {
+          setSnackbarMessage("Edit credentials successful. Please login again");
+          setTimeout(() => {
+            history.push("/");
+            user.dispatch({ type: LOGOUT_SUCCESS, payload: {} });
+          }, 3000);
+        } else {
+          setSnackbarMessage(res.data.message);
+        }
+      })
       .catch((err) => {
         console.log(err);
       });
-
-      };
+  };
 
   return (
-<>
-<br/>
-<br/>
+    <>
+      <br />
+      <br />
       <Typography variant="h4" gutterBottom>
         My Profile
       </Typography>
@@ -54,63 +107,105 @@ const MyProfile = () => {
         <Grid item xs={3}>
           <TextField
             required
-            id={user.username}
+            id="username"
             name="username"
             label="username"
             fullWidth
+            disabled
             autoComplete="username"
             value={inputValues.username}
             onChange={handleChangeInputValues("username")}
           />
         </Grid>
-        <br/>
+        <br />
         <Grid item xs={3}>
           <TextField
             required
-            id={user.email}
+            id="password"
+            name="password"
+            label="password"
+            fullWidth
+            disabled={isEdit ? false : true}
+            autoComplete="password"
+            type={false ? "text" : "password"}
+            value={inputValues.password}
+            onChange={handleChangeInputValues("password")}
+          />
+        </Grid>
+        <br />
+        <Grid item xs={3}>
+          <TextField
+            required
+            id="email"
             name="email"
             label="email"
             fullWidth
+            disabled={isEdit ? false : true}
             autoComplete="email"
             value={inputValues.email}
             onChange={handleChangeInputValues("email")}
           />
         </Grid>
-        <br/>
+        <br />
         <Grid item xs={3}>
           <TextField
             required
-            id={user.address}
+            id="address"
             name="address"
-            label="email"
+            label="address"
             fullWidth
+            disabled={isEdit ? false : true}
             autoComplete="address"
             value={inputValues.address}
             onChange={handleChangeInputValues("address")}
           />
         </Grid>
+        {isInputValid && <Alert severity="warning">{alertMessage}</Alert>}
         <Grid item xs={12}>
           <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={handleEdit}
-                    className={classes.button}
-                    >
-                     Edit        
-                    </Button> <br/><br/>
+            variant="contained"
+            color="primary"
+            onClick={handleEdit}
+            className={classes.button}
+          >
+            Edit
+          </Button>{" "}
+          <br />
+          <br />
           <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={handleSave}
-                    className={classes.button}
-                    >
-                     Save       
-                    </Button>
+            variant="contained"
+            color="primary"
+            onClick={handleSave}
+            className={classes.button}
+          >
+            Save
+          </Button>
         </Grid>
       </Grid>
-</>
+      <Snackbar
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "middle",
+        }}
+        open={open}
+        autoHideDuration={5000}
+        onClose={handleClose}
+        message={snackbarMessage}
+        action={
+          <>
+            <IconButton
+              size="small"
+              aria-label="close"
+              color="inherit"
+              onClick={handleClose}
+            >
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          </>
+        }
+      />
+    </>
   );
-}
-
+};
 
 export default MyProfile;
