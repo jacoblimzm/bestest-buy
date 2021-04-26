c6834cbd32d59ee97f15bb7d2f5375ac7d487fd8
jacoblimzm <jacob_lim@live.com>
2021-04-26 23:42:59 +0800

    add new products and categories

--- models/categoriesSeed.js
+++ models/categoriesSeed.js
@@ -1,32 +1,52 @@
 const categoriesSeed = [
     {
-        image: "https://i.imgur.com/jfvlh2L.jpg",
+        image: "https://i.ibb.co/n8vF6LD/All-01.jpg",
         category: "All",
     },
     {
-        image: "https://i.imgur.com/aotNAsq.jpg",
+        image: "https://i.ibb.co/WBH6GRR/cosmatics-01.jpgpg",
         category: "Beauty & Personal Care",
     },
     {
-        image: "https://i.imgur.com/gXUjbyp.jpg",
+        image: "https://i.ibb.co/CBWDh5q/health-01.jpg",
         category: "Health & Wellness",
     },
     {
-        image: "https://i.imgur.com/0YYWMqv.jpg",
+        image: "https://i.ibb.co/sbX5ZW0/food-01.jpg",
         category: "Food & Beverages",
     },
     {
-        image: "https://i.imgur.com/ORBUYeG.jpg",
+        image: "https://i.ibb.co/YkBBL2H/household-01.jpg",
         category: "Household",
     },
     {
-        image: "https://i.imgur.com/WTpY3hN.jpg",
+        image: "https://i.ibb.co/M9Xk2Sx/electronics-01.jpg",
         category: "Electronics",
     },
     {
-        image: "https://i.imgur.com/jTlZv6H.jpg",
+        image: "https://i.ibb.co/nfWYCj4/fashion-01.jpg",
         category: "Fashion",
-    }]
+    },
+    {
+        image: "https://i.ibb.co/jhjy82q/Pets-01.jpg",
+        category: "Pets",
+    },
+    {
+        image: "https://i.ibb.co/vvfYRKY/sports-01.jpg",
+        category: "Sports & lifestyle",
+    },
+    {
+        image: "https://i.ibb.co/8YZsLMg/toys-01.jpg",
+        category: "Babies & Toys",
+    },
+    {
+        image: "https://i.ibb.co/PZpKDQ9/craft-01.jpg",
+        category: "Hobbies & Craft",
+    },
+    {
+        image: "https://i.ibb.co/sKLbrwV/stationery-01.jpg",
+        category: "Books & Stationeries",
+    },]
 
 
 module.exports = categoriesSeed;
